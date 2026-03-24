65f31aa99453f0b61d4cc841e5bc1ac8137456af
unknown <mayurgiri8907@gmail.com>
2026-03-24 03:17:40 -0700

    add cors

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const connectdb = require('./config/db');
 const userRouter = require('./router/user')
+const cors = require('cors');
 
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectdb = async () => {
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("DB Error:", error.message);
-    process.exit(1);
+    // process.exit(1);
   }
 };
 
