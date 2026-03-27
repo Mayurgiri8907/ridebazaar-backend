03b0a7e6a49aa41fb81c4361e4b8a57c0f477456
unknown <mayurgiri8907@gmail.com>
2026-03-27 02:57:09 -0700

    change auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req,res,next) => {
 
     try {
         
-        const token = req.header('Authorization')?.replace('Bearer ', '');
+        const token = req.header('Authorization')?.replace(' ');
         
         if(!token){
             return res.status(401).json({ message : 'Authentication is required' });
