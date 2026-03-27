ccc5ad2f6a8d4b157bad1912a6ff76e00dc15c6c
unknown <mayurgiri8907@gmail.com>
2026-03-27 04:56:50 -0700

    change auth and error check

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ const auth = async (req,res,next) => {
     try {
         
         const token = req.header('Authorization')?.replace('Bearer ', '');
-        
+        console.log(req);
         if(!token){
             return res.status(401).json({ message : 'Authentication is required' });
         }
--- router/vahical.js
+++ router/vahical.js
@@ -4,7 +4,7 @@ const upload = require('../middleware/upload');
 const vahicalController = require('../controller/vahical');
 const auth = require('../middleware/auth');
 
-router.post('/add', upload.array("images", 4),auth,vahicalController.addvahical)
+router.post("/add", upload.array("images", 4), auth, vahicalController.addvahical);
 // router.post('/singup',vahicalController.usersingup)
 
 module.exports = router;
