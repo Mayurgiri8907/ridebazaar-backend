c2bf6dc63f5c4df3ac62f436eace33b77c4a1c04
unknown <mayurgiri8907@gmail.com>
2026-03-30 04:29:08 -0700

    change all code vahical add images error path

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images/");
+    cb(null, "https://ridebazaar.onrender.com/images/");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
