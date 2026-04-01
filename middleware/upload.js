e0d8565892ea5f1dd0fd1067f92ea32583cbbec9
unknown <mayurgiri8907@gmail.com>
2026-04-01 04:42:40 -0700

    change all code vahical add images error wholecode final error add file

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,12 +1,20 @@
 const multer = require("multer");
+const fs = require("fs");
 const path = require("path");
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+// create folder if not exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images/");
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
