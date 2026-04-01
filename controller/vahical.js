b199708d705b79a322407f11b672abf30b12a175
unknown <mayurgiri8907@gmail.com>
2026-04-01 04:32:48 -0700

    change all code vahical add images error wholecode final error

--- controller/vahical.js
+++ controller/vahical.js
@@ -2,6 +2,9 @@ const vahicalModel = require('../model/vahical');
 
 const addvahical = async (req, res) => {
   try {
+    console.log("BODY:", req.body);
+    console.log("FILE:", req.file);
+
     const { name, description, price, type, fuel } = req.body;
 
     const vahical = new vahicalModel({
@@ -10,13 +13,14 @@ const addvahical = async (req, res) => {
       price,
       type,
       fuel: Array.isArray(fuel) ? fuel : [fuel],
-      image: req.file.filename,
+      image: req.file ? req.file.filename : null,
     });
 
     await vahical.save();
 
     res.json(vahical);
   } catch (err) {
+    console.error("ERROR:", err);
     res.status(500).json({ error: err.message });
   }
 };
